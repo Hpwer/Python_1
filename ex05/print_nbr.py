0831e1f33988144c03ae4c73c046508ab3538887
Hpwer <fantomgri@mail.ru>
2022-03-30 17:01:08 +0300

    print_nbr

--- ex05/print_nbr.py
+++ ex05/print_nbr.py
@@ -1,4 +1,15 @@
 n,m=input().split()
 n=int(n)
 m=int(m)
-print(m*n)
+i = n * m
+e = 1_000_000_000
+while( abs(i)// e == 0 and e > 1):
+    e = e//10
+if i < 0:
+    print("-")
+    i = abs(i)
+while(e > 0):
+    print(i//e%10)
+    e=e//10
+
+    
